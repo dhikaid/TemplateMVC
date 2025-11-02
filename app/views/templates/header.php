9122fe82677d7b8fa5a9f7f26fb679fde09c12df
Bhadrika Aryaputra H <bhadrika.223040018@mail.unpas.ac.id>
2025-11-02 21:01:41 +0700

    feat: database added

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -21,9 +21,7 @@
                 <div class="navbar-nav">
                     <!-- controller -->
                     <a class="nav-link active" aria-current="page" href="<?= BASEURL; ?>">Home</a>
-                    <a class="nav-link" href="<?= BASEURL; ?>/mahasiswa">Mahasiswa</a>
-                    <a class="nav-link" href="<?= BASEURL; ?>/about">About</a>
-
+                    <a class="nav-link" href="<?= BASEURL; ?>/users">Users</a>
                 </div>
             </div>
 
